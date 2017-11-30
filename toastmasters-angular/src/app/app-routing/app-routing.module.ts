23baaa4da58618d773d17c3e2958e0dc40c65039
Terry Palomares <tpalomares18@gmail.com>
2017-11-29 22:32:11 -1000

    added routing and added test html

---
+++ toastmasters-angular/src/app/app-routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TestComponent } from '../components/test/test.component'
+
+const routes: Routes = [
+    {
+        path: 'test',
+        component: TestComponent,
+    },
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes)
+    ],
+    exports: [
+        RouterModule
+    ],
+    declarations: []
+})
+export class AppRoutingModule { }
--- toastmasters-angular/src/app/app.module.ts
+++ toastmasters-angular/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+import { AppRoutingModule } from './app-routing/app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
 
@@ -12,7 +12,8 @@ import { TestComponent } from './components/test/test.component';
     TestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
